36d3456c588082feba7120a803e55e17ba8d4b79
LegenJCdary <jay_cob@interia.pl>
2023-01-30 12:35:44 +0100

    class Upload and Crate stubs in module uploading

---
+++ src/modules/uploading/load.py
@@ -0,0 +1,13 @@
+class Upload:
+
+    def __init__(self):
+        pass
+
+#    def get_boxes(self):
+#        pass
+#
+#    def load_boxes(self):
+#        pass
+
+    def load_box(self):
+        pass
