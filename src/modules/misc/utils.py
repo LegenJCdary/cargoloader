12cd728083fd804d54381164bf9a7d9e606a69fd
LegenJCdary <jay_cob@interia.pl>
2023-01-31 08:59:22 +0100

    Add start_point attribute to InitParams

--- src/modules/misc/utils.py
+++ src/modules/misc/utils.py
@@ -1,4 +1,11 @@
+from datetime import datetime
+
+
 class InitParams:
 
     def __init__(self):
-        pass
+        self.start_point = self.get_start_point()
+
+    @staticmethod
+    def get_start_point() -> str:
+        return datetime.now().strftime("%Y%m%d_%H%M%S")
