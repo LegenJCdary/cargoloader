3770705ca03fc4308d92aba7b82ad769d6937ff8
LegenJCdary <jay_cob@interia.pl>
2023-01-31 13:57:36 +0100

    Add load and validate functions to Conf

--- src/modules/configs/configs.py
+++ src/modules/configs/configs.py
@@ -1,3 +1,9 @@
+from json import loads
+from jsonschema import validate
+
+from . import schemas
+
+
 class Conf:
 
     def __init__(self, conf_path: str, conf_type: str):
@@ -8,3 +14,10 @@ class Conf:
         conf = self.load_conf()
         if self.validate_conf(conf) is None:
             return conf
+
+    def validate_conf(self, conf: dict) -> None:
+        return validate(instance=conf, schema=getattr(schemas, self.conf_type))
+
+    def load_conf(self) -> dict:
+        with open(self.conf_path, "r", encoding="utf-8") as fh:
+            return loads(fh.read())
