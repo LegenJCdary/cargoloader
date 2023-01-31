1ab8f8695e50c49eeb3178f5ed8b39b551a6d848
LegenJCdary <jay_cob@interia.pl>
2023-01-31 13:56:26 +0100

    Add generic loading procedure to Conf class
    Add parse, complete, validate and load methods

--- src/modules/configs/configs.py
+++ src/modules/configs/configs.py
@@ -2,3 +2,18 @@ class Conf:
 
     def __init__(self):
         pass
+
+    def parse_conf(self, conf_path: str) -> dict:
+        conf = self.load_conf(conf_path)
+        if self.validate_conf(conf):
+            return self.complete_conf(conf)
+
+    @staticmethod
+    def load_conf(conf_path: str) -> dict:
+        pass
+
+    def validate_conf(self, conf: dict) -> bool:
+        pass
+
+    def complete_conf(self, conf: dict) -> dict:
+        pass
