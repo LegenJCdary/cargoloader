4871bc84ade6a3bf6c6d23205f0cfe48143edead
LegenJCdary <jay_cob@interia.pl>
2023-01-31 10:12:17 +0100

    Add console handler in Loggers

--- src/modules/outputs/logging.py
+++ src/modules/outputs/logging.py
@@ -4,6 +4,7 @@ import logging
 class Loggers:
 
     main_name = "cargoloader_main_logger"
+    console_fmt = "\n%(asctime)s [%(levelname)s]: %(message)s"
 
     def __init__(self):
         self.logger = self.define_main_logger()
@@ -12,3 +13,8 @@ class Loggers:
         logger = logging.getLogger(self.main_name)
         logger.setLevel(logging.DEBUG)
         return logger
+
+    def add_console_handler(self) -> None:
+        handler = logging.StreamHandler()
+        handler.setFormatter(logging.Formatter(self.console_fmt))
+        self.logger.addHandler(handler)
