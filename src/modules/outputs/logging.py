63764000cdad5a79588ae494113d01f71ecb00a7
LegenJCdary <jay_cob@interia.pl>
2023-01-31 10:17:02 +0100

    Add file handler in Loggers

--- src/modules/outputs/logging.py
+++ src/modules/outputs/logging.py
@@ -39,6 +39,11 @@ class Loggers:
         handler.setFormatter(logging.Formatter(self.main_fmt))
         self.logger.addHandler(handler)
 
+    def add_file_handler(self, log_path: str) -> None:
+        handler = logging.FileHandler(log_path)
+        handler.setFormatter(logging.Formatter(self.main_fmt))
+        self.logger.addHandler(handler)
+
 
 class ColourFormatter(logging.Formatter):
 
