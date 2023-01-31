5f1321bb6d2aafc2831139d1ecec733b09771fe6
LegenJCdary <jay_cob@interia.pl>
2023-01-31 07:22:13 +0100

    Add argument parsing from misc.arguments.CliInput to main

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
-def main():
+from cargoloader.modules.misc.arguments import CliInput
+
+
+def main(cli_options: dict) -> None:
     print("cargoloader initialized")
 
 
 def cargoloader():
-    return main()
+    return main(CliInput.parse_arguments(CliInput()))
 
 
 if __name__ == "__main__":
