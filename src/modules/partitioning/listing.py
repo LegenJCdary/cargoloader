aa603ae3eb2f895785d7ee6f56d56c10fa3e7495
LegenJCdary <jay_cob@interia.pl>
2023-02-02 10:30:33 +0100

    Notes added for ShippingList.calculate_box_size()

--- src/modules/partitioning/listing.py
+++ src/modules/partitioning/listing.py
@@ -8,6 +8,19 @@ class ShippingList:
     def list_container_files(self):
         pass
 
+    """
+    Things to consider:
+    1. Filesystem type.
+    2. Transfer protocol.
+    3. Network card.
+    4. Routing, vlan.
+    5. (SATA) controller.
+    6. Disk type (hw, fs).
+    7. Disk count.
+    8. File sizes.
+    9. Input structure.
+    10. Used tool (other than rsync?)
+    """
     def calculate_box_size(self):
         pass
 
