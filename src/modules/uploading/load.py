1f2891ca1b7eb56236747d4116aeb21e2860a066
LegenJCdary <jay_cob@interia.pl>
2023-02-02 13:23:47 +0100

    Stub Upload class

--- src/modules/uploading/load.py
+++ src/modules/uploading/load.py
@@ -3,11 +3,13 @@ class Upload:
     def __init__(self):
         pass
 
-#    def get_boxes(self):
-#        pass
-#
-#    def load_boxes(self):
-#        pass
+    def get_boxes(self):
+        pass
+
+    def load_boxes(self):
+        for box in self.boxes():
+            self.load_box(box)
+        pass
 
     def load_box(self):
         pass
