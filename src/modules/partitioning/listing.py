92cb48c3aa917de04ce6b32a7fd8ebf49d26ba41
LegenJCdary <jay_cob@interia.pl>
2023-02-02 10:25:32 +0100

    ShippingList class layout

--- src/modules/partitioning/listing.py
+++ src/modules/partitioning/listing.py
@@ -1,4 +1,15 @@
 class ShippingList:
 
-    def __init__(self):
+    def __init__(self, logger, config, docking_list):
+        self.logger = logger
+        self.config = config
+        self.docking_list = docking_list
+
+    def list_container_files(self):
+        pass
+
+    def calculate_box_size(self):
+        pass
+
+    def repackage_to_boxes(self):
         pass
