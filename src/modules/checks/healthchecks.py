e6e8a091ce6e26b2eedc76455155908425e77be2
LegenJCdary <jay_cob@interia.pl>
2023-02-02 13:24:43 +0100

    Stub HealthCheck class

--- src/modules/checks/healthchecks.py
+++ src/modules/checks/healthchecks.py
@@ -2,3 +2,12 @@ class HealthCheck:
 
     def __init__(self):
         pass
+
+    def check_fs(self):
+        pass
+
+    def check_device(self):
+        pass
+
+    def check_loading(self):
+        pass
