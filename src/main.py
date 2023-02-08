6fda5d7ec902f2ac294d2641a37086a8f0d601b4
LegenJCdary <jay_cob@interia.pl>
2023-02-08 15:14:11 +0100

    Import InitParams in main function
    Print init_params to pass pylint

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 from cargoloader.modules.misc.arguments import CliInput
+from cargoloader.modules.misc import utils
 
 
 def main(cli_options: dict) -> None:
+    init_params = utils.InitParams()
     print("cargoloader initialized")
     print(f"with: {cli_options}")
+    print(f"and: {init_params}")
 
 
 def cargoloader():
