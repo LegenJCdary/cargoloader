c4d3c3bff276f8fd74c954267361e448bfbe2b40
LegenJCdary <jay_cob@interia.pl>
2023-02-09 09:31:24 +0100

    Add get_conf_path to Conf

--- src/modules/configs/configs.py
+++ src/modules/configs/configs.py
@@ -1,7 +1,10 @@
 from json import loads
 from jsonschema import validate
+from typing import Union
+import os
 
 from . import schemas
+from ..global_vars import global_vars
 
 
 class Conf:
@@ -21,3 +24,27 @@ class Conf:
     def load_conf(self) -> dict:
         with open(self.conf_path, "r", encoding="utf-8") as fh:
             return loads(fh.read())
+
+    def get_conf_path(self, conf_path: Union[str, bool]) -> str:
+        if not conf_path:
+            try:
+                conf_path = self.get_conf_here()
+                open(conf_path)
+                return conf_path
+            except FileNotFoundError:
+                try:
+                    conf_path = os.path.join(global_vars.application_dir, f"{self.conf_type}.conf")
+                    open(conf_path)
+                    return conf_path
+                except FileNotFoundError:
+                    raise FileNotFoundError(f"[CRITICAL]: {self.conf_type.capitalize()}"
+                                            " configuration could not be obtained.")
+        else:
+            return conf_path
+
+    def get_conf_here(self) -> str:
+        for file in os.listdir(os.getcwd()):
+            if file.endswith(f"{self.conf_type}.conf"):
+                return os.path.join(os.getcwd(), file)
+
+        return ""
